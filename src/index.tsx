288ef28f7681203f9fb9f7140b9373ef47ffd113
W1ckh3ad <69201877+W1ckh3ad@users.noreply.github.com>
2021-10-19 11:51:37 +0200

    trying to do shit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import { render } from "solid-js/web";
-import {
-  defineCustomElements,
-  applyPolyfills,
-} from "@telekom/scale-components/loader";
+import { defineCustomElements } from "@telekom/scale-components/loader";
 import "@telekom/scale-components/dist/scale-components/scale-components.css";
 import "./index.css";
 import App from "./App";
 
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
+defineCustomElements(window);
 
 render(() => <App />, document.getElementById("root") || document.children[0]);
