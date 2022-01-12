4adff7172d8ce2854a911314debf0be6a9c2dea6
A105227211 <A105227211@BE1GC757>
2022-01-12 08:01:20 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: Component = () => {
   const [count, setCount] = createSignal(0);
   return (
     <div class={styles.App}>
-      <scale-button type="button" onClick={() => setCount(count() + 1)}>
+      <scale-button type="button" onClick={() => setCount((count) => count + 1)}>
         {count()}
       </scale-button>
     </div>
