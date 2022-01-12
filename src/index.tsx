3b809e75958ac3da021bbbc3d038b7c3c4e1cfbb
A105227211 <A105227211@BE1GC757>
2022-01-12 08:00:00 +0100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import { render } from "solid-js/web";
-import { defineCustomElements } from "@telekom/scale-components/loader";
-import "@telekom/scale-components/dist/scale-components/scale-components.css";
 import "./index.css";
 import App from "./App";
-
-defineCustomElements(window);
 
 render(() => <App />, document.getElementById("root") || document.children[0]);
